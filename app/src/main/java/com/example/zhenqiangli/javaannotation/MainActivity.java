38561d3269350a4313bd6208973e2ed971cd84de
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-06-03 07:09:03 -0400

    Step 8: Apply annotation

--- app/src/main/java/com/example/zhenqiangli/javaannotation/MainActivity.java
+++ app/src/main/java/com/example/zhenqiangli/javaannotation/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.zhenqiangli.javaannotation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.zhenqiangli.javaannotation.processor.CustomAnnotation;
+
 public class MainActivity extends AppCompatActivity {
 
+    @CustomAnnotation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
